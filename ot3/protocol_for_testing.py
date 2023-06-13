45b3de2f864c9044d70c9e5e38f970905a527a7f
Mewtwoshaurd <shakum281@gmail.com>
2023-06-13 09:25:08 -0400

    Added Logging Functionality to Test Protocol

--- ot3/protocol_for_testing.py
+++ ot3/protocol_for_testing.py
@@ -1,5 +1,10 @@
 from opentrons import protocol_api
 from opentrons import types
+import os
+import subprocess
+
+ALL_LOGS = "/data/logs/all_logs.log"
+CAN_MON = "/data/logs/can_mon.log"
 
 metadata = {
     'protocolName': 'Testing Changes Protocol',
@@ -17,6 +22,17 @@ DRYRUN = 'YES'
 USE_GRIPPER = True
 
 def run(protocol: protocol_api.ProtocolContext):
+    
+    if not protocol.is_simulating():
+        if os.path.exists(ALL_LOGS):
+            os.remove(ALL_LOGS)
+        all_logs = open(ALL_LOGS, "w")
+        if os.path.exists(CAN_MON):
+            os.remove(CAN_MON)
+        can_mon = open(CAN_MON, "w")
+        subprocess.Popen(["journalctl", "-f"], stdout=all_logs)
+        subprocess.Popen(["python3", "-m", "opentrons_hardware.scripts.can_mon"], stdout=can_mon)
+    
     global DRYRUN
 
     def grip_offset(action, item, slot=None):
@@ -90,67 +106,69 @@ def run(protocol: protocol_api.ProtocolContext):
     #tiprack_200 = protocol.load_labware('opentrons_ot3_96_tiprack_200ul', 'D2')
     tiprack_50 = protocol.load_labware('opentrons_ot3_96_tiprack_50ul', 'C2')
 
-    heat_shake = protocol.load_labware('heaterShakerModuleV1', 'D1')
+    heat_shake = protocol.load_module('heaterShakerModuleV1', 'D1')
     plate = heat_shake.load_labware('nest_96_wellplate_100ul_pcr_full_skirt')
-    temp_deck = protocol.load_labware('temperature module gen2', 'D3')
+    temp_deck = protocol.load_module('temperature module gen2', 'D3')
     thermocycler = protocol.load_module('thermocycler module gen2')
     # pipette
     p50 = protocol.load_instrument("p50_single_gen3", "left", tip_racks=[tiprack_50])
 
-    heat_shake.open_labware_latch()
-    thermocycler.open_lid()
-    protocol.pause("Ready!")
-    heat_shake.close_labware_latch()
+    for i in range(3):
 
-    p50.pick_up_tip()
-    p50.move_to(plate['A1'].top())
-    p50.return_tip()
+        heat_shake.open_labware_latch()
+        thermocycler.open_lid()
+        #protocol.pause("Ready!")
+        heat_shake.close_labware_latch()
 
-    heat_shake.open_labware_latch()
-    protocol.move_labware(
-        labware = plate,
-        new_location = temp_deck,
-        use_gripper = USE_GRIPPER,
-        use_pick_up_location_lpc_offset=True,
-        use_drop_location_lpc_offset=True,
-        pick_up_offset=grip_offset("pick-up", "heater-shaker",1),
-        drop_offset=grip_offset("drop", "temp-module"),
-    )
-    heat_shake.close_labware_latch()
+        p50.pick_up_tip()
+        p50.move_to(plate['A1'].top())
+        p50.return_tip()
 
-    p50.pick_up_tip()
-    p50.move_to(plate['A1'].top())
-    p50.return_tip()
+        heat_shake.open_labware_latch()
+        protocol.move_labware(
+            labware = plate,
+            new_location = temp_deck,
+            use_gripper = USE_GRIPPER,
+            use_pick_up_location_lpc_offset=True,
+            use_drop_location_lpc_offset=True,
+            pick_up_offset=grip_offset("pick-up", "heater-shaker",1),
+            drop_offset=grip_offset("drop", "temp-module"),
+        )
+        heat_shake.close_labware_latch()
 
-    thermocycler.open_lid()
-    protocol.move_labware(
-        labware = plate,
-        new_location = thermocycler,
-        use_gripper = USE_GRIPPER,
-        use_pick_up_location_lpc_offset=True,
-        use_drop_location_lpc_offset=True,
-        pick_up_offset=grip_offset("pick-up", "temp-module"),
-        drop_offset=grip_offset("drop", "thermo-cycler"),
-    )
+        p50.pick_up_tip()
+        p50.move_to(plate['A1'].top())
+        p50.return_tip()
 
-    p50.pick_up_tip()
-    p50.move_to(plate['A1'].top())
-    p50.return_tip()
+        thermocycler.open_lid()
+        protocol.move_labware(
+            labware = plate,
+            new_location = thermocycler,
+            use_gripper = USE_GRIPPER,
+            use_pick_up_location_lpc_offset=True,
+            use_drop_location_lpc_offset=True,
+            pick_up_offset=grip_offset("pick-up", "temp-module"),
+            drop_offset=grip_offset("drop", "thermo-cycler"),
+        )
 
-    thermocycler.close_lid()
-    protocol.delay(seconds=20)
-    thermocycler.open_lid()
+        p50.pick_up_tip()
+        p50.move_to(plate['A1'].top())
+        p50.return_tip()
 
-    heat_shake.open_labware_latch()
-    protocol.move_labware(
-        labware = plate,
-        new_location = heat_shake,
-        use_gripper = USE_GRIPPER,
-        use_pick_up_location_lpc_offset=True,
-        use_drop_location_lpc_offset=True,
-        pick_up_offset=grip_offset("pick-up", "thermo-cycler"),
-        drop_offset=grip_offset("drop", "heater-shakers", 1),
-    )
+        thermocycler.close_lid()
+        #protocol.delay(seconds=20)
+        thermocycler.open_lid()
+
+        heat_shake.open_labware_latch()
+        protocol.move_labware(
+            labware = plate,
+            new_location = heat_shake,
+            use_gripper = USE_GRIPPER,
+            use_pick_up_location_lpc_offset=True,
+            use_drop_location_lpc_offset=True,
+            pick_up_offset=grip_offset("pick-up", "thermo-cycler"),
+            drop_offset=grip_offset("drop", "heater-shaker", 1),
+        )
 
 
 
