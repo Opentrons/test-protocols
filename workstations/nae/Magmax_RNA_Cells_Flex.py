2ab9723d774d98f691e2f5f7c2f5bd4c3b80216c
Sara Kowalski <53955458+skowalski08@users.noreply.github.com>
2023-09-18 11:52:22 -0400

    Update Magmax_RNA_Cells_Flex.py

--- workstations/nae/Magmax_RNA_Cells_Flex.py
+++ workstations/nae/Magmax_RNA_Cells_Flex.py
@@ -75,7 +75,7 @@ def run(ctx):
     tips_sn = tips200.wells()[:num_samples]
 
     # load P1000M pipette
-    m1000 = ctx.load_instrument('p1000_multi_gen3', 'left')
+    m1000 = ctx.load_instrument('flex_8channel_1000', 'left')
 
     """
     Here is where you can define the locations of your reagents.
