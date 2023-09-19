0dcd31134e92a15dfcbe8162c0aaa4b936a05917
Sara Kowalski <53955458+skowalski08@users.noreply.github.com>
2023-09-19 11:22:36 -0400

    update pipette load name

--- workstations/nae/ZymoBIOMICS_Magbead_DNA_Cells_Flex.py
+++ workstations/nae/ZymoBIOMICS_Magbead_DNA_Cells_Flex.py
@@ -77,7 +77,7 @@ def run(ctx):
     tips = [*tips1000.wells()[num_samples:96],*tips1001.wells(),*tips1002.wells()]
     tips_sn = tips1000.wells()[:num_samples]
     # load instruments
-    m1000 = ctx.load_instrument('p1000_multi_gen3', 'left')
+    m1000 = ctx.load_instrument('flex_8channel_1000', 'left')
 
     """
     Here is where you can define the locations of your reagents.
