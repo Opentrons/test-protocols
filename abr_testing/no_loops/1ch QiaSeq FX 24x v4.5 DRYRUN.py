811467140de91c0f8cfc81f88b1daf54b5d651af
Sara Kowalski <sara.kowalski@opentrons.com>
2023-09-27 11:53:26 -0400

    corrected errors

--- abr_testing/no_loops/1ch QiaSeq FX 24x v4.5 DRYRUN.py
+++ abr_testing/no_loops/1ch QiaSeq FX 24x v4.5 DRYRUN.py
@@ -239,7 +239,7 @@ def run(protocol: protocol_api.ProtocolContext):
                     protocol.delay(seconds=0.2)
                     p50.default_speed = 400
                     protocol.comment('--> Adding Barcodes')
-                    p50.aspirate(BarcodeVol, reagent_plate.wells_by_name()[barcodes[loop]].bottom(z=0.5), rate=0.25)
+                    p50.aspirate(BarcodeVol, reagent_plate.wells_by_name()[barcodes[loop]].bottom(.5), rate=0.25)
                     p50.dispense(LIGVol+BarcodeVol, sample_plate_1[X].bottom(z=1), rate=0.25)
                     p50.move_to(sample_plate_1[X].bottom(z=1))
                     p50.mix(LIGMixRep,LIGMixVol, rate=0.5)
@@ -254,7 +254,7 @@ def run(protocol: protocol_api.ProtocolContext):
                 for loop, X in enumerate(column_1_list):
                     tipcheck()
                     p50.pick_up_tip()
-                    p50.aspirate(BarcodeVol, reagent_plate.wells_by_name()[barcodes[loop]].bottom(z=0.5), rate=0.25)
+                    p50.aspirate(BarcodeVol, reagent_plate.wells_by_name()[barcodes[loop]].bottom(.5), rate=0.25)
                     p50.dispense(BarcodeVol, sample_plate_1.wells_by_name()[X].bottom(1))
                     p50.mix(BarcodeMixRep,BarcodeMixVol)
                     p50.return_tip() if TIP_TRASH == False else p50.drop_tip()
