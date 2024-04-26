c9ca5cd26a66c3f058251f888de386391136f1dd
rclarke0 <rhyann.clarke@opentrons.com>
2024-04-26 16:36:26 -0400

    script for tip splash test

--- abr_testing/Liquid-Setups/Trash Chute Testing/Filled Tip Splash Testing.py
+++ abr_testing/Liquid-Setups/Trash Chute Testing/Filled Tip Splash Testing.py
@@ -1,11 +1,8 @@
 from opentrons import protocol_api
-from opentrons import types
-
-import random
 
 metadata = {
-    'protocolName': 'Throw out Liquid Filled Tips',
-    'author': 'Rhyann clarke <rhyann.clarke@opentrons.com>',
+    'protocolName': 'Throw out Liquid Filled Tips (1000, 200, 50)',
+    'author': 'Rhyann Clarke <rhyann.clarke@opentrons.com>',
     'source': 'Protocol Library',
 }
 requirements = {
@@ -14,13 +11,26 @@ requirements = {
 }
 
 def run(protocol: protocol_api.ProtocolContext):
+    # Waste Chute
     waste_chute = protocol.load_waste_chute()
     
     # Initiate Labware
-    tiprack_1000        = protocol.load_labware(load_name='opentrons_flex_96_tiprack_1000ul', location='D1') # Tip Rack
-    master_reservoir    = protocol.load_labware('axygen_1_reservoir_90ml', 'C2')
-    p1000               = protocol.load_instrument(instrument_name='flex_8channel_1000', mount='left', tip_racks=[tiprack_1000]) # Pipette
-
-    p1000.pick_up_tip()
-    p1000.aspirate(500, master_reservoir['A1'].bottom(z=0.5))
-    p1000.drop_tip()
+    tiprack_1000        = protocol.load_labware(load_name='opentrons_flex_96_tiprack_1000ul', location='A2', adapter = "opentrons_flex_96_tiprack_adapter") 
+    tiprack_200         = protocol.load_labware(load_name='opentrons_flex_96_tiprack_200ul', location='B2', adapter = "opentrons_flex_96_tiprack_adapter")
+    tiprack_50         = protocol.load_labware(load_name='opentrons_flex_96_tiprack_50ul', location='C2', adapter = "opentrons_flex_96_tiprack_adapter")
+    master_reservoir    = protocol.load_labware(load_name = 'axygen_1_reservoir_90ml', location='D2')
+    # Pipette
+    p96               = protocol.load_instrument(instrument_name='flex_96channel_1000', mount='left', tip_racks=[tiprack_1000, tiprack_200, tiprack_50]) 
+    
+    # Protocol Steps
+    p96.pick_up_tip(tiprack_50)
+    p96.aspirate(50, master_reservoir['A1'].bottom(z=1))
+    p96.drop_tip()
+    
+    p96.pick_up_tip(tiprack_200)
+    p96.aspirate(200, master_reservoir['A1'].bottom(z=1))
+    p96.drop_tip()
+    
+    p96.pick_up_tip(tiprack_1000)
+    p96.aspirate(1000, master_reservoir['A1'].bottom(z=1))
+    p96.drop_tip()
--- abr_testing/Static Testing Scripts/Pick Up 50 uL Tips.py
+++ abr_testing/Static Testing Scripts/Pick Up 50 uL Tips.py
@@ -2,7 +2,7 @@ from opentrons import protocol_api
 from opentrons import types
 
 metadata = {
-    'protocolName': 'Pick Up 50 uL Tips Right and Left',
+    'protocolName': 'Pick Up 50 uL Tips Right and Left - 5 racks',
     'author': 'Rhyann Clarke <rhyann.clarke@opentrons.com>',
     'source': 'Protocol Library'
     }
@@ -13,37 +13,23 @@ requirements = {
 }
 
 def run(protocol: protocol_api.ProtocolContext):
-    # DECK SETUP AND LABWARE 
-    reagent_plate       = protocol.load_labware('nest_96_wellplate_2ml_deep', location = 'D2')
-    
-    tiprack_50_1        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'C2')
+    # DECK SETUP AND LABWARE     
+    tiprack_50_1        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'A2')
     tiprack_50_2        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'B2')
-    tiprack_50_3        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'A2')
-    tiprack_50_4        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'C3')
-    tiprack_50_5        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'B3')
-    
-    
-    # ========== THIRD ROW ===========
-    thermocycler        = protocol.load_module('thermocycler module gen2')
+    tiprack_50_3        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'C2')
+    tiprack_50_4        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'B3')
+    tiprack_50_5        = protocol.load_labware('opentrons_flex_96_tiprack_50ul', location = 'C3')
 
-    # pipette
-    p50 = protocol.load_instrument("flex_8channel_50", "right", tip_racks=[tiprack_50_1,tiprack_50_2, tiprack_50_3])
-    p1000 = protocol.load_instrument("flex_8channel_1000", "left", tip_racks=[tiprack_50_1,tiprack_50_2, tiprack_50_3])
+    # Pipette
+    p50 = protocol.load_instrument("flex_8channel_50", "right", tip_racks=[tiprack_50_1,tiprack_50_2, tiprack_50_3, tiprack_50_4, tiprack_50_5])
+    p1000 = protocol.load_instrument("flex_8channel_1000", "left", tip_racks=[tiprack_50_1,tiprack_50_2, tiprack_50_3, tiprack_50_4, tiprack_50_5])
     
-    column_list   = ['A1','A2','A3','A4','A5','A6', 'A7','A8','A9','A10','A11','A12']
-    thermocycler.open_lid()
-    for i in list(range(3)):
-        for n in column_list:
+    for i in list(range(4)):
+        for i in list(range(60)):
             p50.pick_up_tip()
-            p50.aspirate(30, reservoir_12well[n].bottom(z = 1))
-            p50.dispense(10, reagent_plate[n].bottom(z = 0.5))
-            p50.dispense(10, reservoir_96well[n].bottom(z = 0.5))
             p50.return_tip()
-    p50.reset_tipracks()
-    for i in list(range(3)):
-        for n in column_list:
+        p50.reset_tipracks()
+        for i in list(range(60)):
             p1000.pick_up_tip()
-            p1000.aspirate(30, reservoir_12well[n].bottom(z = 1))
-            p1000.dispense(10, reagent_plate[n].bottom(z = 0.5))
-            p1000.dispense(10, reservoir_96well[n].bottom(z = 0.5))
             p1000.return_tip()
+        p1000.reset_tipracks()
