ddbaab35c97c9f498de6e4ee2c4b6ac31c830bb6
Nicholas Shiland <nicholas.shiland@opentrons.com>
2024-05-30 12:39:09 -0400

    tip_pickup_qol

--- abr_testing/Static Testing Scripts/Pick Up 50 uL Tips.py
+++ abr_testing/Static Testing Scripts/Pick Up 50 uL Tips.py
@@ -2,7 +2,7 @@ from opentrons import protocol_api
 from opentrons import types
 
 metadata = {
-    'protocolName': 'Pick Up Tips Right and Left - 5 racks',
+    'protocolName': 'Pick Up Lotta Tips - 5 racks',
     'author': 'Nick Shiland <nicholas.shiland@opentrons.com>',
     'source': 'Protocol Library'
     }
@@ -24,6 +24,7 @@ def add_parameters(parameters: protocol_api.Parameters):
     ],
     default = "opentrons_flex_96_tiprack_50ul"
     )
+
     parameters.add_str(
         variable_name="left_pipette",
         display_name="Pipette Type Left Mount",
@@ -48,6 +49,29 @@ def add_parameters(parameters: protocol_api.Parameters):
     ],
     default = "flex_8channel_1000"
     )
+
+    parameters.add_int(
+        variable_name="tip_repeats",
+        display_name="Tip Rack Repeats",
+        description="Set Number of Times to Repeat Racks",
+        default = 5,
+        minimum = 1,
+        maximum = 100,
+        unit = "times",   
+    )
+    
+    parameters.add_str(
+        variable_name="which_pipette",
+        display_name="Which Pipette(s) to Use",
+        description="Set Which Pipette(s) to Use",
+        choices=[
+        {"display_name": "Left Only", "value": "left"},
+        {"display_name": "Right Only", "value": "right"},
+        {"display_name": "Both", "value": "both"},
+    ],
+    default = "left"
+    )
+    
     parameters.add_bool(
         variable_name= "tip_selection",
         display_name="Use Entire Tip Rack?",
@@ -60,31 +84,42 @@ def run(protocol: protocol_api.ProtocolContext):
     right_pipette = protocol.params.right_pipette
     tip_size = protocol.params.tip_size
     tip_selection = protocol.params.tip_selection
+    tip_repeats = protocol.params.tip_repeats
+    which_pipette = protocol.params.which_pipette
     # DECK SETUP AND LABWARE     
-    tiprack_1        = protocol.load_labware(tip_size, location = 'A2')
-    tiprack_2        = protocol.load_labware(tip_size, location = 'B2')
-    tiprack_3        = protocol.load_labware(tip_size, location = 'C2')
-    tiprack_4        = protocol.load_labware(tip_size, location = 'B3')
-    tiprack_5        = protocol.load_labware(tip_size, location = 'C3')
+    tiprack_1        = protocol.load_labware(tip_size, location = 'A1')
+    tiprack_2        = protocol.load_labware(tip_size, location = 'B1')
+    tiprack_3        = protocol.load_labware(tip_size, location = 'C1')
+    tiprack_4        = protocol.load_labware(tip_size, location = 'A2')
+    tiprack_5        = protocol.load_labware(tip_size, location = 'B2')
 
     # Pipette
     pleft = protocol.load_instrument(left_pipette, "left", tip_racks=[tiprack_1,tiprack_2, tiprack_3, tiprack_4, tiprack_5])
     pright = protocol.load_instrument(right_pipette, "right", tip_racks=[tiprack_1,tiprack_2, tiprack_3, tiprack_4, tiprack_5])
     
     if tip_selection == True:
-        for i in list(range(4)):
-            for i in list(range(60)):
-                pleft.pick_up_tip()
-                pleft.return_tip()
-            pleft.reset_tipracks()
-            for i in list(range(60)):
-                pright.pick_up_tip()
-                pright.return_tip()
-            pright.reset_tipracks()
+        if which_pipette == "left" or "both":
+            for i in list(range(tip_repeats - 1)):
+                for i in list(range(60)):
+                    pleft.pick_up_tip()
+                    pleft.return_tip()
+                pleft.reset_tipracks()
+        if which_pipette == "right" or "both":
+            for i in list(range(tip_repeats - 1)):    
+                for i in list(range(60)):
+                    pright.pick_up_tip()
+                    pright.return_tip()
+                pright.reset_tipracks()
 
     else:
-        for i in list(range(60)):
-            pleft.pick_up_tip()
-            pleft.return_tip()
-            pleft.reset_tipracks()
-        
+        if which_pipette == "right":
+            for i in list(range(12*tip_repeats)):
+                pright.pick_up_tip()
+                pright.return_tip()
+                pright.reset_tipracks()
+
+        else:
+            for i in list(range(12*tip_repeats)):
+                pleft.pick_up_tip()
+                pleft.return_tip()
+                pleft.reset_tipracks()
