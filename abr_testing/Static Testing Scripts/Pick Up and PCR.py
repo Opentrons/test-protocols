b1c80adafa8db4ba7bcc0e332a6c57dbc7f5b31e
Nicholas Shiland <nicholas.shiland@opentrons.com>
2024-06-04 16:27:36 -0400

    remove_trash_liquidsetup

--- abr_testing/Static Testing Scripts/Pick Up and PCR.py
+++ abr_testing/Static Testing Scripts/Pick Up and PCR.py
@@ -13,6 +13,16 @@ requirements = {
     "robotType": "OT-3",
     "apiLevel": "2.18",
 }
+def add_parameters(parameters: protocol_api.Parameters):
+    parameters.add_int(
+        variable_name="repetitions",
+        display_name="Cycle Repetitions",
+        description="Set Number of Times to Repeat Cycle",
+        default = 5,
+        minimum = 1,
+        maximum = 150,
+        unit = "times",
+    )
 
 def run(protocol: protocol_api.ProtocolContext):
     # DECK SETUP AND LABWARE    
@@ -23,20 +33,26 @@ def run(protocol: protocol_api.ProtocolContext):
     tiprack_5        = protocol.load_labware("opentrons_flex_96_tiprack_50ul", location = 'B2')
     pcr_plate        = protocol.load_labware('opentrons_96_wellplate_200ul_pcr_full_skirt', location = 'B3')
     reservoir        = protocol.load_labware('nest_12_reservoir_15ml', location = 'D3')
-
+    repetitions = protocol.params.repetitions
     # Pipette
     pleft = protocol.load_instrument("flex_8channel_1000", "left", tip_racks=[tiprack_1,tiprack_2, tiprack_3, tiprack_4, tiprack_5])
    
-    for i in list(range(5)):
+   
+    for i in list(range(repetitions)):
         for i in list(range(60)):
-            pleft.pick_up_tip()
-            pleft.aspirate(40, pcr_plate["A1"])
-            pleft.move_to(reservoir["A1"].bottom())
-            pleft.dispense(40, pcr_plate["A1"])
-            pleft.return_tip()
+            pleft.transfer(
+                volume=50,
+                dest=pcr_plate["A1"],
+                source=reservoir["A1"],
+                touch_tip=True,
+                trash = False,
+            )
         pleft.reset_tipracks()
 
     #pcr_plate["A1"].top()
+    #add mix
         
-
+        #pleft.aspirate(40, pcr_plate["A1"])
+        #pleft.move_to(reservoir["A1"].bottom())
+        #pleft.dispense(40, pcr_plate["A1"])
     #touch tip with z offset using fur inside a 1well
